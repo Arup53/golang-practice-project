f1025b8eaa6c9947e9e1f9169c023e71efd915dc
Arup53 <arupkumarhowlader12@gmail.com>
2025-08-14 10:58:52 +0600

    implement object to message passing through channel in concept module

--- Concepts/main.go
+++ Concepts/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // why receiver function needs to be pointer receiver in go
 // from oops sense, we are directly  modifing the properties of a object so, in go we use pointer receiver or return pointer to modify the struct(properties)
@@ -23,6 +26,46 @@ type Pond struct {
 type KoiList []Koi
 
 type ShapesList []Shapes
+
+type Account struct {
+	ID int
+	balance int
+}
+
+type Transaction struct{
+	accountID int
+	txnType   string
+	ammount   int
+}
+
+func testChannelAsArgument(testChan <-chan int, num int ){
+
+	msgValue := <-testChan
+    
+	fmt.Println("I got your message bro ", msgValue)
+	sum := msgValue+num;
+	fmt.Println("sum", sum)
+}
+
+func testChannelAsArgumentMultipleValueReceiver(values <-chan Transaction ){
+    for value := range values{
+       msg :=value
+	   fmt.Println("Hey bro my msg is", msg.txnType, "and my id is ", msg.accountID, "and ammount ", msg.ammount)
+	}
+}
+
+// sender go routine
+func (acc *Account) accountOperation(wg *sync.WaitGroup,txnChan chan<- Transaction, operation string, txnAmmount int){
+   defer wg.Done() 
+   
+   txnChan <- Transaction{
+	accountID: acc.ID,
+	txnType: operation,
+	ammount: txnAmmount,
+   }
+
+   fmt.Println("I have sent my message", acc.ID)
+}
 
 func main() {
 
@@ -54,7 +97,58 @@ func main() {
      
     
 	// fmt.Println(koiArrays2)
-	x:=2
-	px:= &x
-	fmt.Println(*px)
+	// ---------- Pointer -----------
+	// x:=2
+	// px:= &x
+	// fmt.Println("value", *px , "address of", px)
+
+    
+    //  channel 
+
+	// testChan := make(chan int)
+
+	// go testChannelAsArgument(testChan,101)
+
+	// testChan <- 10
+    // fmt.Println("All task done")
+    
+	// txnChan := make(chan Transaction)
+
+
+	// go testChannelAsArgumentMultipleValueReceiver(txnChan)
+
+	// go func ()  {
+	// 	txnChan <- Transaction{accountID: 1, txnType: "deposit", ammount: 500}
+	// }()
+	// go func ()  {
+	// 	txnChan <- Transaction{accountID: 3, txnType: "withdraw", ammount: 1000}
+	// }()
+	// go func ()  {
+	// 	txnChan <- Transaction{accountID: 2, txnType: "deposit", ammount: 3000}
+	// }()
+  
+	// time.Sleep(1* time.Second)
+	// close(txnChan)
+	// fmt.Println("All transaction done")
+
+	// -------- object to channel message passing ------
+    
+	txnChan := make(chan Transaction)
+    var wg sync.WaitGroup
+
+	user1 := Account{ID: 2, balance: 2000}
+	user2 := Account{ID: 1, balance: 100}
+	user3 := Account{ID: 3, balance: 5000}
+    
+	go testChannelAsArgumentMultipleValueReceiver(txnChan)
+    wg.Add(3)
+	go user1.accountOperation(&wg,txnChan, "withdraw", 500)
+	go user2.accountOperation(&wg,txnChan, "deposit",1000)
+	go user3.accountOperation(&wg,txnChan, "withdraw",2000)
+
+	
+	wg.Wait()
+	close(txnChan)
+	
+	fmt.Println("All transaction done")
 }
--- concurrent-transaction/main.go
+++ concurrent-transaction/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type BankAccount struct {
 	accountID int
@@ -14,6 +17,46 @@ type Transaction struct {
     amount    int
 	resultChan chan string
 }
+
+func processTransaction(trxChan <-chan Transaction, accounts []BankAccount) {
+	for trx := range trxChan { // Loop until channel is closed
+		// Find the target account
+		var account *BankAccount
+		for i := range accounts {
+			if accounts[i].accountID == trx.accountID {
+				account = &accounts[i]
+				break
+			}
+		}
+
+		if account == nil {
+			trx.resultChan <- "Account not found"
+			continue
+		}
+
+		// Lock the account mutex and defer unlock
+		account.mutex.Lock()
+		func() {
+			defer account.mutex.Unlock() // Ensures mutex is released even on early return
+
+			switch trx.txnType {
+			case "deposit":
+				account.balance += trx.amount
+				trx.resultChan <- "Deposit successful, new balance: " + fmt.Sprint(account.balance)
+			case "withdraw":
+				if account.balance >= trx.amount {
+					account.balance -= trx.amount
+					trx.resultChan <- "Withdrawal successful, new balance: " + fmt.Sprint(account.balance)
+				} else {
+					trx.resultChan <- "Withdrawal failed: insufficient funds"
+				}
+			default:
+				trx.resultChan <- "Unknown transaction type"
+			}
+		}()
+	}
+}
+
 
 
 

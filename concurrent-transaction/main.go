1904f5a77b3b95364a7c25f407d1c16abd18bb1f
Arup53 <arupkumarhowlader12@gmail.com>
2025-08-19 13:11:26 +0600

    implement blocking concurrency operation

--- concurrent-transaction/main.go
+++ concurrent-transaction/main.go
@@ -18,59 +18,117 @@ type Transaction struct {
 	resultChan chan string
 }
 
-func processTransaction(trxChan <-chan Transaction, accounts []BankAccount) {
-	for trx := range trxChan { // Loop until channel is closed
-		// Find the target account
-		var account *BankAccount
-		for i := range accounts {
-			if accounts[i].accountID == trx.accountID {
-				account = &accounts[i]
-				break
-			}
-		}
+// func processTransaction(trxChan <-chan Transaction, accounts []BankAccount) {
+// 	for trx := range trxChan { // Loop until channel is closed
+// 		// Find the target account
+// 		var account *BankAccount
+// 		for i := range accounts {
+// 			if accounts[i].accountID == trx.accountID {
+// 				account = &accounts[i]
+// 				break
+// 			}
+// 		}
 
-		if account == nil {
-			trx.resultChan <- "Account not found"
+// 		if account == nil {
+// 			trx.resultChan <- "Account not found"
+// 			continue
+// 		}
+
+// 		// Lock the account mutex and defer unlock
+// 		account.mutex.Lock()
+// 		func() {
+// 			defer account.mutex.Unlock() // Ensures mutex is released even on early return
+
+// 			switch trx.txnType {
+// 			case "deposit":
+// 				account.balance += trx.amount
+// 				trx.resultChan <- "Deposit successful, new balance: " + fmt.Sprint(account.balance)
+// 			case "withdraw":
+// 				if account.balance >= trx.amount {
+// 					account.balance -= trx.amount
+// 					trx.resultChan <- "Withdrawal successful, new balance: " + fmt.Sprint(account.balance)
+// 				} else {
+// 					trx.resultChan <- "Withdrawal failed: insufficient funds"
+// 				}
+// 			default:
+// 				trx.resultChan <- "Unknown transaction type"
+// 			}
+// 		}()
+// 	}
+// }
+
+
+func preocessTransaction(trnChan <-chan Transaction, accounts map[int]*BankAccount){
+
+	for trx := range trnChan {
+        account, exist := accounts[trx.accountID]
+		
+		if !exist{
+			trx.resultChan <-"Account not found"
 			continue
 		}
 
-		// Lock the account mutex and defer unlock
 		account.mutex.Lock()
-		func() {
-			defer account.mutex.Unlock() // Ensures mutex is released even on early return
-
-			switch trx.txnType {
-			case "deposit":
+		func ()  {
+		defer account.mutex.Unlock()
+		
+		switch trx.txnType{
+		case "deposit":
 				account.balance += trx.amount
-				trx.resultChan <- "Deposit successful, new balance: " + fmt.Sprint(account.balance)
+				trx.resultChan<-fmt.Sprintf(
+                    "Deposit successful, new balance: %d", account.balance,
+                )
+			
 			case "withdraw":
-				if account.balance >= trx.amount {
-					account.balance -= trx.amount
-					trx.resultChan <- "Withdrawal successful, new balance: " + fmt.Sprint(account.balance)
+				if trx.amount <= account.balance {
+                      account.balance -= trx.amount
+					  trx.resultChan<-fmt.Sprintf(
+                    "Withdraw successful, new balance: %d", account.balance,
+                )
 				} else {
-					trx.resultChan <- "Withdrawal failed: insufficient funds"
+					trx.resultChan <-"Withdrawal failed: insufficient funds"
 				}
 			default:
-				trx.resultChan <- "Unknown transaction type"
-			}
+				 trx.resultChan <- "Unknown transaction type"
+
+		}
 		}()
 	}
 }
 
 
 
-
-
 func main() {
 	trxChan := make(chan Transaction)
     
-	accountSlice := []BankAccount{
-		{accountID: 1, balance: 5000}, 
-		{accountID: 2, balance: 3000},
-		{accountID: 3, balance: 7000},
+	// accountSlice := []BankAccount{
+	// 	{accountID: 1, balance: 5000}, 
+	// 	{accountID: 2, balance: 3000},
+	// 	{accountID: 3, balance: 7000},
+	// }
+
+	accounts := map[int]*BankAccount{
+		1:{accountID: 1, balance: 5000}, 
+		2:{accountID: 2,balance: 300},
+		3:{accountID: 3, balance: 7000},
 	}
 
+	go preocessTransaction(trxChan, accounts)
+    
+	// ------------------ Blocking operation i.e. although processTransaction runs spearatly main go routine prints result one after another not true concurrency------------- 
+	result1:= make(chan string)
+	trxChan <- Transaction{accountID: 1, txnType: "deposit", amount: 1000, resultChan: result1}
+	fmt.Println(<-result1)
+      
+    result3:=make(chan string)
+	trxChan <-Transaction{accountID: 3, txnType: "withdraw", amount: 10000, resultChan: result3}
+	fmt.Println(<-result3)
+
+	result2:= make(chan string)
+	trxChan<-Transaction{accountID: 2,txnType: "withdraw", amount: 100, resultChan: result2}
+    fmt.Println(<-result2)
 	
-	
+
+	close(trxChan)
 }
 
